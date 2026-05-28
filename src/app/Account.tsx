33e40329bd28a6e0e220db1bca5f9e7a178ae473
Toh Wei Jun <13tohweijun@gmail.com>
2026-05-28 15:25:56 +0800

    added avatar upload

--- src/app/Account.tsx
+++ src/app/Account.tsx
@@ -1,46 +1,50 @@
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import { View, Alert, TextInput, Text, TouchableOpacity } from 'react-native'
-import Avatar from './Avatar'
-import { appStyles } from './lib/styles'
-// ...
+import { useEffect, useState } from "react";
+import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import Avatar from "./Avatar";
+import { appStyles } from "./lib/styles";
+import { supabase } from "./lib/supabase";
 
-
-export default function Account({ userId, email }: { userId: string; email?: string }) {
-  const [loading, setLoading] = useState(true)
-  const [username, setUsername] = useState('')
-  const [website, setWebsite] = useState('')
-  const [avatarUrl, setAvatarUrl] = useState('')
-  const styles = appStyles
+export default function Account({
+  userId,
+  email,
+}: {
+  userId: string;
+  email?: string;
+}) {
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState("");
+  const [website, setWebsite] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const styles = appStyles;
 
   useEffect(() => {
-    if (userId) getProfile()
-  }, [userId])
+    if (userId) getProfile();
+  }, [userId]);
 
   async function getProfile() {
     try {
-      setLoading(true)
+      setLoading(true);
 
       let { data, error, status } = await supabase
-        .from('profiles')
+        .from("profiles")
         .select(`username, website, avatar_url`)
-        .eq('id', userId)
-        .single()
+        .eq("id", userId)
+        .single();
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
       }
     } catch (error) {
       if (error instanceof Error) {
-        Alert.alert(error.message)
+        Alert.alert(error.message);
       }
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -49,12 +53,12 @@ export default function Account({ userId, email }: { userId: string; email?: str
     website,
     avatar_url,
   }: {
-    username: string
-    website: string
-    avatar_url: string
+    username: string;
+    website: string;
+    avatar_url: string;
   }) {
     try {
-      setLoading(true)
+      setLoading(true);
 
       const updates = {
         id: userId,
@@ -62,29 +66,36 @@ export default function Account({ userId, email }: { userId: string; email?: str
         website,
         avatar_url,
         updated_at: new Date(),
-      }
+      };
 
-      let { error } = await supabase.from('profiles').upsert(updates)
+      let { error } = await supabase.from("profiles").upsert(updates);
 
       if (error) {
-        throw error
+        throw error;
       }
     } catch (error: any) {
-      Alert.alert(error.message)
+      Alert.alert(error.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   return (
     <View style={styles.container}>
       <View>
-
-        {/* ... */}
-
+        <Avatar
+          size={200}
+          url={avatarUrl}
+          onUpload={(url: string) => {
+            setAvatarUrl(url);
+            updateProfile({ username, website, avatar_url: url });
+          }}
+        />
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
         <Text style={styles.label}>Email</Text>
         <TextInput
-          value={email ?? ''}
+          value={email ?? ""}
           editable={false}
           selectTextOnFocus={false}
           style={[styles.input, styles.inputDisabled]}
@@ -93,7 +104,7 @@ export default function Account({ userId, email }: { userId: string; email?: str
       <View style={styles.verticallySpaced}>
         <Text style={styles.label}>Username</Text>
         <TextInput
-          value={username || ''}
+          value={username || ""}
           onChangeText={(text) => setUsername(text)}
           style={styles.input}
         />
@@ -101,7 +112,7 @@ export default function Account({ userId, email }: { userId: string; email?: str
       <View style={styles.verticallySpaced}>
         <Text style={styles.label}>Website</Text>
         <TextInput
-          value={website || ''}
+          value={website || ""}
           onChangeText={(text) => setWebsite(text)}
           style={styles.input}
         />
@@ -110,18 +121,25 @@ export default function Account({ userId, email }: { userId: string; email?: str
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <TouchableOpacity
           style={[styles.button, loading && styles.buttonDisabled]}
-          onPress={() => updateProfile({ username, website, avatar_url: avatarUrl })}
+          onPress={() =>
+            updateProfile({ username, website, avatar_url: avatarUrl })
+          }
           disabled={loading}
         >
-          <Text style={styles.buttonText}>{loading ? 'Loading ...' : 'Update'}</Text>
+          <Text style={styles.buttonText}>
+            {loading ? "Loading ..." : "Update"}
+          </Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.verticallySpaced}>
-        <TouchableOpacity style={styles.button} onPress={() => supabase.auth.signOut()}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => supabase.auth.signOut()}
+        >
           <Text style={styles.buttonText}>Sign Out</Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
+  );
 }
--- src/app/Avatar.tsx
+++ src/app/Avatar.tsx
@@ -1,31 +1,119 @@
-import React from 'react'
-import { Image, View, StyleSheet } from 'react-native'
+import * as ImagePicker from "expo-image-picker";
+import { useEffect, useState } from "react";
+import { Alert, Image, Text, TouchableOpacity, View } from "react-native";
+import { appStyles } from "./lib/styles";
+import { supabase } from "./lib/supabase";
 
-type Props = {
-  url?: string | null
-  size?: number
+interface Props {
+  size: number;
+  url: string | null;
+  onUpload: (filePath: string) => void;
 }
 
-export default function Avatar({ url, size = 80 }: Props) {
+export default function Avatar({ url, size = 150, onUpload }: Props) {
+  const [uploading, setUploading] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+  const avatarSize = { height: size, width: size };
+  const styles = appStyles;
+
+  useEffect(() => {
+    if (url) downloadImage(url);
+  }, [url]);
+
+  async function downloadImage(path: string) {
+    try {
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .download(path);
+
+      if (error) {
+        throw error;
+      }
+
+      const fr = new FileReader();
+      fr.readAsDataURL(data);
+      fr.onload = () => {
+        setAvatarUrl(fr.result as string);
+      };
+    } catch (error: any) {
+      console.log("Error downloading image: ", error.message);
+    }
+  }
+
+  async function uploadAvatar() {
+    try {
+      setUploading(true);
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images, // Restrict to only images
+        allowsMultipleSelection: false, // Can only select one image
+        allowsEditing: true, // Allows the user to crop / rotate their photo before uploading it
+        quality: 1,
+        exif: false, // We don't want nor need that data.
+      });
+
+      if (result.canceled || !result.assets || result.assets.length === 0) {
+        console.log("User cancelled image picker.");
+        return;
+      }
+
+      const image = result.assets[0];
+      console.log("Got image", image);
+
+      if (!image.uri) {
+        throw new Error("No image uri!"); // Realistically, this should never happen, but just in case...
+      }
+
+      const arraybuffer = await fetch(image.uri).then((res) =>
+        res.arrayBuffer(),
+      );
+
+      const fileExt = image.uri?.split(".").pop()?.toLowerCase() ?? "jpeg";
+      const path = `${Date.now()}.${fileExt}`;
+      const { data, error: uploadError } = await supabase.storage
+        .from("avatars")
+        .upload(path, arraybuffer, {
+          contentType: image.mimeType ?? "image/jpeg",
+        });
+
+      if (uploadError) {
+        throw uploadError;
+      }
+
+      onUpload(data.path);
+    } catch (error: any) {
+      if (error) {
+        Alert.alert(error.message);
+      } else {
+        throw error;
+      }
+    } finally {
+      setUploading(false);
+    }
+  }
+
   return (
-    <View style={[styles.container, { width: size, height: size, borderRadius: size / 2 }]}> 
-      {url ? (
-        <Image source={{ uri: url }} style={[styles.image, { width: size, height: size, borderRadius: size / 2 }]} />
+    <View style={styles.avatarContainer}>
+      {avatarUrl ? (
+        <Image
+          source={{ uri: avatarUrl }}
+          accessibilityLabel="Avatar"
+          style={[avatarSize, styles.avatar, styles.image]}
+        />
       ) : (
-        <View style={[styles.placeholder, { width: size, height: size, borderRadius: size / 2 }]} />
+        <View style={[avatarSize, styles.avatar, styles.noImage]} />
       )}
+      <View>
+        <TouchableOpacity
+          style={[styles.button, uploading && styles.buttonDisabled]}
+          onPress={uploadAvatar}
+          disabled={uploading}
+        >
+          <Text style={styles.buttonText}>
+            {uploading ? "Uploading ..." : "Upload"}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    overflow: 'hidden',
-  },
-  image: {
-    resizeMode: 'cover',
-  },
-  placeholder: {
-    backgroundColor: '#cbd5e1',
-  },
-})
